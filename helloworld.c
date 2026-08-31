0f6878653db868078103d64cf9e15e65fb84d6a8
Basstt_ElSevic <2911312474@qq.com>
2026-08-31 18:47:59 +0800

    构造前馈函数

--- helloworld.c
+++ helloworld.c
@@ -1,7 +1,22 @@
 #include "stdio.h"
+
+#define WIDTH 20
+#define HEIGHT 20
+
+float feed_forward(float inputs[HEIGHT][WIDTH], float weights[HEIGHT][WIDTH])
+{
+	float output = 0.0f;
+	for (int y = 0; y < HEIGHT; ++y) {
+		for (int x = 0; x < WIDTH; ++x) {
+			output += inputs[y][x] * weights[y][x];
+		}
+	}
+	return output;
+}
+
 int main ()
 {
 	printf("Hello World!\n");
-	printf("Hello its Basstt Here!\n");
+	
 	return 0;
-} 
+}  
